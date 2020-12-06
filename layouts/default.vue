38fef3da66d04657039eff203d0eef4e668c18fe
LukaszRatajczyk <lukasz@ahola.pl>
2020-12-06 14:40:02 +0100

    Coming soon page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
   <v-app>
     <nuxt />
+    <v-footer app color="transparent">
+      <v-img
+        src="/ahola_logo_by.svg"
+        contain
+        height="16px"
+        class="ma-4"
+      ></v-img>
+    </v-footer>
   </v-app>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <v-containter>
+  <v-containter fluid>
+    <v-row justify="center" align="center" style="height: 100vh">
+      <v-col class="text-center">
+        <v-img
+          src="/todoodly.svg"
+          max-height="96px"
+          class="ma-6"
+          contain
+        ></v-img>
+        <h4 class="text-h4 mt-6 font-weight-light text--secondary">
+          Coming soon
+        </h4>
+      </v-col>
+    </v-row>
     <!-- Code here -->
   </v-containter>
 </template>
